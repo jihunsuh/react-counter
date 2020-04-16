51cfcd6bce34f94c16e6c271fbf9f86f569e5621
서지훈 <jihunsuh12@gmail.com>
2020-04-16 13:35:40 +0900

    react-counter app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+    this.increaseCount = this.increaseCount.bind(this);
+    this.decreaseCount = this.decreaseCount.bind(this);
+  }
+
+  increaseCount(e) {
+    e.preventDefault();
+    // state의 count를 1 증가시킨다
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  }
+
+  decreaseCount(e) {
+    e.preventDefault();
+    // state의 count를 1 감소시킨다
+    this.setState((state) => {
+      if (state.count >= 1) {
+        return {
+          count: state.count - 1,
+        };
+      }
+    });
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount 실행됨');
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate 실행됨');
+    // this.setState((state) => ({
+    //   count: state.count + 1,
+    // }));
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount 실행됨');
+  }
+
+  render() {
+    let { count } = this.state;
+    let { increaseCount, decreaseCount } = this;
+    return (
+      <div>
+        <div id="counter">count: {count}</div>
+        <div id="buttons">
+          <button id="increase" onClick={increaseCount}>
+            increase
+          </button>
+          <button id="decrease" onClick={decreaseCount}>
+            decrease
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
